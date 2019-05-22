64c9c11de77408ab743d3103b959d52fb40b14b2
Shani Rubovitch <shani.rubovitch@bitctrl.de>
2019-05-22 12:54:24 +0200

    addional base case for $ char

--- regex.js
+++ regex.js
@@ -14,7 +14,7 @@ function search(pattern, text) {
 
 function match(pattern, text) {
     if (pattern === '') return true // base case for recursion: when the pattern is empty every text fits it
-
+    if (pattern === '$' && text === '') return true //  additional base case for the $ char
     else return matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice[1])
 }
 
